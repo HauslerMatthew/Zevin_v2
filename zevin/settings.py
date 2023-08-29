622bcfc5114ec244bd83a07c31a054a6fa929d41
Matthew Hausler <74732297+HauslerMatthew@users.noreply.github.com>
2023-08-29 18:31:48 -0500

    Update settings.py

--- zevin/settings.py
+++ zevin/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4)h3h)tbn$d080$dbtosk#=nx#*0g_nz@-p*a(opqjl#sy93-_'
+SECRET_KEY = 'sk_test_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
